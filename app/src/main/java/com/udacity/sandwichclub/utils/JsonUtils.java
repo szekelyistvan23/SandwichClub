65baf338c13ea927caf354601680f1dc6687ba95
Szekely Istvan <steven.sekler@gmail.com>
2018-02-18 19:16:29 +0200

    feat:JsonUtil class implementation

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,48 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
-    public static Sandwich parseSandwichJson(String json) {
-        return null;
+    public static Sandwich parseSandwichJson(String json) throws JSONException {
+        Sandwich sandwich = new Sandwich();
+        JSONObject sandwichDetails = new JSONObject(json);
+
+        JSONObject temporalName = new JSONObject(sandwichDetails.getString("name"));
+        JSONArray ingredients = sandwichDetails.getJSONArray("ingredients");
+        JSONArray knownAs = temporalName.getJSONArray("alsoKnownAs");
+
+        sandwich.setMainName(temporalName.getString("mainName"));
+        sandwich.setAlsoKnownAs(jsonArrayToStringArray(knownAs));
+        sandwich.setPlaceOfOrigin(sandwichDetails.getString("placeOfOrigin"));
+
+        if (sandwichDetails.getString("placeOfOrigin").equals("")){
+            sandwich.setPlaceOfOrigin("unknown");
+        }
+
+        sandwich.setDescription(sandwichDetails.getString("description"));
+        sandwich.setImage(sandwichDetails.getString("image"));
+        sandwich.setIngredients(jsonArrayToStringArray(ingredients));
+
+        return sandwich;
+    }
+    private static List<String> jsonArrayToStringArray(JSONArray array){
+        ArrayList<String> temporalArray = new ArrayList<>();
+        if (array != null && array.length()> 0){
+            for (int i = 0; i < array.length(); i++){
+                try {
+                    temporalArray.add(array.getString(i));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+      return temporalArray;
     }
 }
